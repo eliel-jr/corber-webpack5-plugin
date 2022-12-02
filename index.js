264a5d0ce85a30388f4e5fec1df8e6f9208081bb
Eliel Silverio Junior <elieljr@systemar.com.br>
2022-12-02 13:37:46 -0300

    [FIX] Verify exists process.env.CORBER_PLATFORM to execute plugin

--- index.js
+++ index.js
@@ -5,23 +5,25 @@ const cordovaAssetTree   = require('corber/lib/targets/cordova/utils/cordova-ass
 
 class CorberWebpackPlugin {
   apply(compiler) {    
-    let context = compiler.context;
-    // let platform = process.argv.includes('--CORBER_PLATFORM=android') ? 'android' : 'ios';
     let platform = process.env.CORBER_PLATFORM;
-    let cdvAssets = cordovaAssetTree.getPaths(platform, './corber/cordova');
+    if (platform) {
+      let context = compiler.context;
+      // let platform = process.argv.includes('--CORBER_PLATFORM=android') ? 'android' : 'ios';
+      let cdvAssets = cordovaAssetTree.getPaths(platform, './corber/cordova');
 
-    cdvAssets.files.forEach((file) => {
-      if (file === 'plugins/**') {
-        //need to build the tree until corber/cordova/utils/cordova-assets is upgraded
-        let plugins = glob.sync(path.join('./corber/cordova', cdvAssets.assetsPath, 'plugins/**/*.js'));
-        plugins.forEach((plugin) => {              
-          new EntryPlugin(context, path.join(process.cwd(), plugin), plugin).apply(compiler);
-        });
-      } else {
-        let filePath = path.join(process.cwd(), './corber/cordova', cdvAssets.assetsPath, file);
-        new EntryPlugin(context, filePath, file).apply(compiler);
-      }
-    });
+      cdvAssets.files.forEach((file) => {
+        if (file === 'plugins/**') {
+          //need to build the tree until corber/cordova/utils/cordova-assets is upgraded
+          let plugins = glob.sync(path.join('./corber/cordova', cdvAssets.assetsPath, 'plugins/**/*.js'));
+          plugins.forEach((plugin) => {              
+            new EntryPlugin(context, path.join(process.cwd(), plugin), plugin).apply(compiler);
+          });
+        } else {
+          let filePath = path.join(process.cwd(), './corber/cordova', cdvAssets.assetsPath, file);
+          new EntryPlugin(context, filePath, file).apply(compiler);
+        }
+      });
+    }
   }
 }
 
